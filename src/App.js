f5b23f6ff1faf5963dff9cfed61c72f2fb3815d0
daonguyendev <daonguyen.dev@gmail.com>
2023-09-15 14:30:30 +0700

    Hello C03

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello C03, welcome to ReactJS World ^^
+    </div>
+  );
+}
+
+export default App;
